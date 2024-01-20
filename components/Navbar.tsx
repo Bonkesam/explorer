486b6f6e6f7fe3fc03f578cb8436cd82e6255329
Bonkesam <bonkesam9@gmail.com>
2024-01-20 14:01:12 +0300

    Adds the button component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { NAV_LINKS } from "@/constants";
 
 const Navbar = () => {
   return (
@@ -7,6 +8,16 @@ const Navbar = () => {
         <Link href="/">
             <Image src="/hilink-logo.svg" alt="logo" width={74} height={29}/>
         </Link>
+        <ul className="hidden h-full gap-12 lg:flex">
+            {NAV_LINKS.map((link) => (
+                <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transision-all hover:font-bold">
+                    {link.label}
+                </Link>
+            ))}
+        </ul>
+        <div className="lg:flexcenter hidden">
+
+        </div>
     </nav>
   )
 }
